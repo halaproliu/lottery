f8ea80b6e7b2f0b0a1331ecdd28c6886d6820f48
luffy <luffybryant@gmail.com>
2022-01-10 23:08:46 +0800

    修改docker脚本

--- sh/server.sh
+++ sh/server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 networkName=$1
 docker build -t server -f Dockerfile.node .
-docker run -d --network $networkName -p 3001:3001 --restart always server
+docker run -d --network $networkName -p 3001:3001 server
