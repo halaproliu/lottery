d785e0d27d60dbec8ccb41b685292253457850fc
liuwenjian <liuwenjian@bilibili.com>
2022-01-11 10:50:07 +0800

    修改部署端口

--- sh/server.sh
+++ sh/server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 networkName=$1
-outerPort=8000
+outerPort=3001
 innerPort=3001
 docker build -t server -f Dockerfile.node .
 docker run -d --network $networkName -p $outerPort:$innerPort --restart always server
