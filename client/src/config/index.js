96828f63727c715a028c6be57c705f5d9c6a17f4
liuwenjian <liuwenjian@bilibili.com>
2022-01-11 11:11:08 +0800

    修改部署脚本

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,7 +1,7 @@
 const env = process.env.NODE_ENV
 const config = {
-  baseURL: env === 'production' ? 'http://124.223.2.144' : 'http://localhost:3001'
-  // baseURL: 'http://localhost:3001'
+  // baseURL: env === 'production' ? 'http://124.223.2.144' : 'http://localhost:3001'
+  baseURL: 'http://localhost:3001'
 }
 
 export default config
